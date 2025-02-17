ac492996a7591667263a469090c5c2b43c4d39f3
elva <elva@aa.bb>
2025-02-17 14:54:47 +0800

    update:test

---
+++ lang/vendor/filament/zh_TW/buttons.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'id' => 'ID',
+    'sort' => '序號',
+    'name' => '名稱',
+    'title' => '標題',
+    'category' => '類別',
+    'description' => '描述',
+    'content' => '內容',
+    'key' => 'Key',
+    'image' => '圖片',
+    'img' => '圖示',
+    'state' => '狀態',
+    'published_at' => '發佈時間',
+    'created_at' => '創建時間',
+    'updated_at' => '更新時間',
+    'game_type' => '遊戲分類',
+];
